ab2b8764d6572eb22b23d34660baee4a08779e02
wfb <1948728748@qq.com>
2017-11-14 23:52:55 +0800

    第一次嵌入

---
+++ src/components/NavBread.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="temp">
+        <div class="nav-breadcrumb-wrap">
+            <div class="container">
+                <nav class="nav-breadcrumb">
+                    <a href="/">Home</a>
+                   <slot name="bread"></slot>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'temp',
+        data () {
+            return {
+                //数据
+            }
+        }
+    }
+</script>
