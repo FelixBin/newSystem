2484e5896e841025e5d00664f175648cd974e593
wfb <1948728748@qq.com>
2017-12-05 17:29:11 +0800

    在main.js中注册vuex

--- src/main.js
+++ src/main.js
@@ -5,16 +5,35 @@ import App from './App'
 import router from './router'
 import vueInfiniteScroll from  'vue-infinite-scroll'
 import axios from 'axios';
+import  Vuex from 'vuex'
 import {currency} from './util/currency'
 axios.defaults.withCredentials = true;
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 Vue.use(vueInfiniteScroll);
+Vue.use(Vuex);
 Vue.filter("currency", currency);
+
+const store = new Vuex.Store({
+    state: {
+        nickName: '',
+        cartCount: 0
+    },
+    mutations: {
+        updateUserInfo(state, nickName){
+            state.nickName = nickName;
+        },
+        updateCartCount(state,cartCount){
+            state.cartCount+=cartCount;
+        }
+    }
+})
+
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
+    store,
     template: '<App/>',
     components: {App}
 })
